9627953df104fba97cb79156df4f86212c94dee9
qiaochao <qiaochao@icourt.cc>
2023-08-06 21:54:01 +0800

    feat : webview

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({
@@ -12,13 +13,15 @@ class DetailPage extends StatelessWidget {
     // // 通过settings属性来获取传递的参数
     // final argument = route?.settings.arguments;
     final argument = ModalRoute.of(context)?.settings.arguments;
-    return Scaffold(
+    return WebviewScaffold(
+      /** 
       appBar: AppBar(
-        title: Text('Detail Page'),
+        title: Text('WebView'),
       ),
-      body: Center(
-        child: Text('This is the detail page$argument'),
-      ),
+      */
+      url: '$argument', // 设置要加载的网址
+      withZoom: true, // 是否允许缩放，默认为true
+      withLocalStorage: true, // 是否启用本地存储，默认为true
     );
   }
 }
